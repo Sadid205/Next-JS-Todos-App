7c8e2f1dba8db1eaebda3bc8bd1c7e551d1dcee9
Abdullah-Al-Sadid <abdullahalsadid1914@gmail.com>
2023-07-11 12:39:01 +0600

    initial

---
+++ app/header.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { LogoutBtn } from "../components/Clients";
+
+const header = () => {
+  return (
+    <div className="header">
+      <div>
+        <h2>Todo.</h2>
+      </div>
+      <article>
+        <Link href={"/"}>Home</Link>
+        <Link href={"/profile"}>Profile</Link>
+        <LogoutBtn />
+      </article>
+    </div>
+  );
+};
+
+export default header;
